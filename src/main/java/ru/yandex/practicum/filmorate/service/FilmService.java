ff5bfaa6735ee378b90a98ac3dfeb5773226b024
KonneyJ <konnova.j@yandex.ru>
2024-10-13 13:52:31 +0300

    Исправлена ошибка checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
